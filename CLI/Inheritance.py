3e21aacc7243e5d91e6852280a80be8e4929d2cd
AshishSondagar <Candy128x>
2018-08-19 13:01:09 +0530

    inherit type2

--- CLI/Inheritance.py
+++ CLI/Inheritance.py
@@ -4,3 +4,21 @@ class Test:
 
 obj = Test()
 obj.fun()
+'''
+OutPut:
+Hello
+'''
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def sayHi(self):
+        print('Hey My Name is name', self.name)
+
+p = Person('Dev Kumar')
+p.sayHi()
+'''
+OutPut:
+Hey My Name is name Dev Kumar
+'''
