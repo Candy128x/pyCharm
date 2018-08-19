0c6f9ad44ab8fd1a23f1c8ce697c401632b882ad
AshishSondagar <Candy128x>
2018-08-19 13:22:36 +0530

    inherit 3type

--- CLI/Inheritance.py
+++ CLI/Inheritance.py
@@ -9,6 +9,7 @@ OutPut:
 Hello
 '''
 
+
 class Person:
     def __init__(self, name):
         self.name = name
@@ -21,4 +22,35 @@ p.sayHi()
 '''
 OutPut:
 Hey My Name is name Dev Kumar
+'''
+
+
+class CSStudent:
+    stream = 'cse';
+
+    def __init__(self, roll):
+        self.roll = roll
+
+    def setAddrs(self, addrs):
+        self.addrs = addrs
+
+    def getAddrs(self):
+        return self.addrs
+
+a = CSStudent(101)
+b = CSStudent(102)
+a.setAddrs('Mumbai, Maharastra.')
+
+print(a.stream)
+print(b.stream)
+print(a.roll)
+print(CSStudent.stream)
+print(a.getAddrs())
+'''
+OutPut:
+cse
+cse
+101
+cse
+Mumbai, Maharastra.
 '''
